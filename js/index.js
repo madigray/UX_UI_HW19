db0559e5fa974ae89f4496049f52234814989924
madigray <madi.gray.m@gmail.com>
2022-04-19 14:28:16 -0700

    3 years later
    *read in Spongebob narrator voice*

---
+++ js/index.js
@@ -0,0 +1,16 @@
+ScrollReveal().reveal('.skills_img', {delay: 250, reset: true});
+ScrollReveal().reveal('.skills_text', {delay: 500, reset: true});
+
+ScrollReveal().reveal('.services h2', {delay: 250, reset: true});
+ScrollReveal().reveal('.card2', {delay: 300, reset: true});
+ScrollReveal().reveal('.card1', {delay: 450, reset: true});
+ScrollReveal().reveal('.card3', {delay: 600, reset: true});
+
+ScrollReveal().reveal('.portfolio h2', {delay: 250, reset: true});
+ScrollReveal().reveal('.project2', {delay: 300, reset: true});
+ScrollReveal().reveal('.project1', {delay: 450, reset: true});
+ScrollReveal().reveal('.project3', {delay: 600, reset: true});
+
+ScrollReveal().reveal('.footer img', {delay: 400, reset: true});
+ScrollReveal().reveal('.footer h2', {delay: 550, reset: true});
+ScrollReveal().reveal('.footer p', {delay: 600, reset: true});
